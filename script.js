58ffadb4a80d265ef6bfb7f07b0e478af2fdfea2
RishikaSoni16 <135811075+RishikaSoni16@users.noreply.github.com>
2024-05-09 15:47:41 +0600

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+var targetElement = document.getElementById('level');
+var domLevel = 1;
+
+while (targetElement !== document.documentElement) {
+    targetElement = targetElement.parentElement;
+    domLevel++;
+}
+
+alert('The level of the element is: ' + domLevel);
